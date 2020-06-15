b9bb856ba0001b56bc99350f2ec0c0773c2dc93c
Alexandr Polyakov <PolyakovQA@yandex.ru>
2020-06-15 18:59:10 +0300

    add class from package (manager)

---
+++ src/main/java/ru/netology/domain/vk/manager/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology.domain.vk.manager;
+
+import ru.netology.domain.vk.Post;
+
+public class PostManager {
+    private Post[] posts;
+    public Post[] search(int idOwner, String domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+    public void delete(int idOwner, int postId) {
+
+    }
+}
