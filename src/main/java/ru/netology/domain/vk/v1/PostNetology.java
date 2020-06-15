6fc34f908ebd04797cc470355958dc70929160b9
Alexandr Polyakov <PolyakovQA@yandex.ru>
2020-06-15 19:27:21 +0300

    #V1

---
+++ src/main/java/ru/netology/domain/vk/v1/PostNetology.java
@@ -0,0 +1,15 @@
+package ru.netology.domain.vk.v1;
+
+public class PostNetology {
+    private int id;
+    private String image;
+    private int date;
+    private String text;
+    private String commentsInfo;
+    private boolean openPost;
+    private boolean closePost;
+    private boolean deletePost;
+    private boolean editPost;
+    private String likePost;
+
+}
